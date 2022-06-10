20d4da3e0268f5f2d541f46fd0521c468eb1704c
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-06-10 16:46:21 -0500

    initial setup

---
+++ src/components/web/Home/NosotrosHome/NosotrosHome.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Row, Col } from "react-bootstrap";
+
+import './NosotrosHome.scss'
+
+export default function NosotrosHome() {
+  return (
+    <Container fluid className="nosotros-home">
+        <Row className='nosotros-home-titulo'>
+          <h1>Nosotros</h1>
+        </Row>
+    </Container>
+  )
+}
