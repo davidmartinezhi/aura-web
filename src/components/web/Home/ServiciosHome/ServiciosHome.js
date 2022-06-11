da9ca762ac516cb8c6561068613b88ab0d3220ec
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-06-10 19:41:37 -0500

    terminada sección demo

--- src/components/web/Home/ServiciosHome/ServiciosHome.js
+++ src/components/web/Home/ServiciosHome/ServiciosHome.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import testPhoto from "../../../../assets/img/stars.jpg";
+import brandingPhoto from "../../../../assets/img/jpg/branding.jpg";
+import brandingPhoto2 from "../../../../assets/img/jpg/branding2.jpg";
+import marketingPhoto from "../../../../assets/img/jpg/marketing.jpg";
+import webPhoto from "../../../../assets/img/jpg/webdev.jpg";
+import softwarePhoto from "../../../../assets/img/jpg/software.jpg";
 import "./ServiciosHome.scss";
 
 export default function ServiciosHome() {
@@ -8,8 +14,69 @@ export default function ServiciosHome() {
       <Row className="servicios-home-titulo pt-5 pb-5">
         <h1>Servicios</h1>
       </Row>
-
-      {/* Branding */}
+      <Row className="servicios-home-servicio">
+      <Col sm={5} lg={3}>
+          <Card >
+            <Card.Img variant="top" src={brandingPhoto} />
+            <Card.Body>
+              <Card.Title><h2>Branding</h2></Card.Title>
+              <Card.Text>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.</p>
+              </Card.Text>
+              <Button><span>Conocer más</span></Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col sm={5} lg={3}>
+          <Card >
+            <Card.Img variant="top" src={marketingPhoto} />
+            <Card.Body>
+              <Card.Title><h2>Márketing digital</h2></Card.Title>
+              <Card.Text>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.</p>
+              </Card.Text>
+              <Button><span>Conocer más</span></Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col sm={5} lg={3}>
+          <Card >
+            <Card.Img variant="top" src={webPhoto} />
+            <Card.Body>
+              <Card.Title><h2>Desarrollo Web</h2></Card.Title>
+              <Card.Text>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.</p>
+              </Card.Text>
+              <Button><span>Conocer más</span></Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col sm={5} lg={3}>
+          <Card>
+            <Card.Img variant="top" src={softwarePhoto} />
+            <Card.Body>
+              <Card.Title><h2>Software a la medida</h2></Card.Title>
+              <Card.Text>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.</p>
+              </Card.Text>
+              <Button><span>Conocer más</span></Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      {/* 
       <Row className="servicios-home-servicio">
       <Col className="p-3">
             <h2>Branding</h2>
@@ -18,7 +85,6 @@ export default function ServiciosHome() {
         <Col></Col>
       </Row>
 
-      {/* Márketing digital */}
       <Row className="servicios-home-servicio">
 
         <Col></Col>
@@ -27,7 +93,6 @@ export default function ServiciosHome() {
             <p className="m-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </Col>
       </Row>
-      {/* Desarrollo Web */}
       <Row className="servicios-home-servicio">
       <Col className="p-3">
             <h2>Desarrollo Web y Aplicaciones</h2>
@@ -35,7 +100,7 @@ export default function ServiciosHome() {
         </Col>
         <Col></Col>
       </Row>
-      {/* Desarrollo de Software */}
+      
       <Row className="servicios-home-servicio">
 
         <Col></Col>
@@ -43,7 +108,7 @@ export default function ServiciosHome() {
             <h2>Software a la medida</h2>
             <p className="m-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </Col>
-      </Row>
+      </Row> */}
     </Container>
   );
 }
