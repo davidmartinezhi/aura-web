9b71e5f8042e53f05919aa2b7f97a2916da16301
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-06-11 03:28:40 -0500

    agregado logo

--- src/components/web/Footer/Footer.js
+++ src/components/web/Footer/Footer.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import logoAuraWhite from "../../../assets/img/logo/Blanco/Aura_Blanco.png";
+
+import "./Footer.scss";
 
 export default function Footer() {
   return (
-    <div><h1>Footer</h1></div>
-  )
+    <Container fluid className="footer">
+      <Row>
+        <Col className="brand">
+          <img className="brand-logo" src={logoAuraWhite} alt="Aura logo" />
+        </Col>
+      </Row>
+      <Row className="footer-row">
+        <Col className="footer-row-col">
+          {" "}
+          <h1>Social Media</h1>
+        </Col>
+        <Col className="footer-row-col">
+          {" "}
+          <h1>Newsletter</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
