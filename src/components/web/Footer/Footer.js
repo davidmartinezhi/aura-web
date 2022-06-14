cd13e778ce5386a1c0a8b4e25525025f3c2dfcdf
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-06-13 20:07:48 -0500

    initial setup footer

--- src/components/web/Footer/Footer.js
+++ src/components/web/Footer/Footer.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import logoAuraWhite from "../../../assets/img/logo/Blanco/Aura_Blanco.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faInstagram,
+    faFacebook,
+  } from "@fortawesome/free-brands-svg-icons";
+
+import Newsletter from "../Newsletter/Newsletter";
 
 import "./Footer.scss";
 
@@ -15,11 +22,45 @@ export default function Footer() {
       <Row className="footer-row">
         <Col className="footer-row-col">
           {" "}
-          <h1>Social Media</h1>
+          <h1>Newsletter</h1>
+          <Newsletter />
         </Col>
         <Col className="footer-row-col">
-          {" "}
-          <h1>Newsletter</h1>
+          <h1>Contáctanos</h1>
+          <div className="social-media">
+            {" "}
+            <a
+              href="https://www.facebook.com/auraconsultoriamx"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+            <a
+              href="https://www.instagram.com/aura_consultoria"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </div>
+          <div className="contact-info">
+            {" "}
+            <a
+              href="mailto:auraconsultoriayservicios@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span>auraconsultoriayservicios@gmail.com</span>
+            </a>
+            <a
+              href="tel:+528711169694"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span>+52 871 116 9694</span>
+            </a>
+          </div>
         </Col>
       </Row>
     </Container>
---
+++ src/components/web/Newsletter/Newsletter.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+
+export default function Newsletter() {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Correo electrónico</Form.Label>
+        <Form.Control type="email" placeholder="Correo electrónico" />
+        <Form.Text className="text-muted">Ingresa</Form.Text>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
