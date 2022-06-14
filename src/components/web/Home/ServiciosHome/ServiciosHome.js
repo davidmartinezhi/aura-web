32030fac7aa78941332210b7dd68c1f4e75ac961
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-06-14 17:56:37 -0500

    definido contenido de servicios

--- src/components/web/Home/ServiciosHome/ServiciosHome.js
+++ src/components/web/Home/ServiciosHome/ServiciosHome.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import testPhoto from "../../../../assets/img/stars.jpg";
-import brandingPhoto from "../../../../assets/img/jpg/branding.jpg";
-import brandingPhoto2 from "../../../../assets/img/jpg/branding2.jpg";
-import marketingPhoto from "../../../../assets/img/jpg/marketing.jpg";
+import consultingPhoto from "../../../../assets/img/jpg/consultory.jpg";
+import brandingPhoto2 from "../../../../assets/img/jpg/branding.jpg";
+import marketingPhoto from "../../../../assets/img/jpg/marketing2.jpg";
 import webPhoto from "../../../../assets/img/jpg/webdev.jpg";
 import softwarePhoto from "../../../../assets/img/jpg/software.jpg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -12,24 +12,36 @@ import "./ServiciosHome.scss";
 
 export default function ServiciosHome() {
   return (
-    <Container fluid className="servicios-home">
+    <Container fluid className="servicios-home" id="servicios">
       <Row className="servicios-home-titulo pt-5">
         <h1>Servicios</h1>
       </Row>
       <Row className="servicios-home-servicio">
+      <Col sm={5} lg={3}>
+          <Card>
+            <Card.Img variant="top" src={consultingPhoto} />
+            <Card.Body>
+              <Card.Title><h3>Consultoría</h3></Card.Title>
+              <Card.Text>
+                <p>Comprometidos con encontrar <b>Las Soluciones Específica Que Tu Negocio Necesita</b>. <br/>Generamos estrategias que buscan generar mejores rendimientos para tu empresa, utilizando los servicios digitales que ofrecemos.</p>
+              </Card.Text>
+              <div className="more-btn">
+              <Button><span>Conocer más</span> <FontAwesomeIcon icon={faArrowRight}/> </Button>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
       <Col sm={5} lg={3}>
           <Card >
             <Card.Img variant="top" src={brandingPhoto2} />
             <Card.Body>
               <Card.Title><h3>Branding</h3></Card.Title>
               <Card.Text>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.</p>
+                <p>Encontramos la identidad unica de tu empresa para de esta manera generar <b>Valor y Rentabilidad</b> a tu marca y nos encargamos de todo proceso.<br/> Incluyendo proceso creativo y tramites de registro e identidad
+empresarial.</p>
               </Card.Text>
               <div className="more-btn">
-              <Button className='mt-2'><span>Conocer más</span> <FontAwesomeIcon icon={faArrowRight}/> </Button>
+              <Button><span>Conocer más</span> <FontAwesomeIcon icon={faArrowRight}/> </Button>
               
               </div>
               
@@ -42,10 +54,8 @@ export default function ServiciosHome() {
             <Card.Body>
               <Card.Title><h3>Márketing Digital</h3></Card.Title>
               <Card.Text>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.</p>
+                <p>Aplicamos estrategias de comercialización llevadas a cabo en los medios digitales. 
+                <br/>Generamos estrategias especificas para cada uno de nuestros clientes, ofreciendo así; <b>Ideas Que Rompen Redes</b>.</p>
               </Card.Text>
               <div className="more-btn">
               <Button><span>Conocer más</span> <FontAwesomeIcon icon={faArrowRight}/> </Button>
@@ -57,29 +67,9 @@ export default function ServiciosHome() {
           <Card >
             <Card.Img variant="top" src={webPhoto} />
             <Card.Body>
-              <Card.Title><h3>Aplicaciones Web</h3></Card.Title>
-              <Card.Text>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.</p>
-              </Card.Text>
-              <div className="more-btn">
-              <Button><span>Conocer más</span> <FontAwesomeIcon icon={faArrowRight}/> </Button>
-              </div>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col sm={5} lg={3}>
-          <Card>
-            <Card.Img variant="top" src={softwarePhoto} />
-            <Card.Body>
               <Card.Title><h3>Desarrollo de Software</h3></Card.Title>
               <Card.Text>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.</p>
+                <p>Desarrollo integral de codigo para <b>Crear, Actualizar y/o Optimizar</b> el corazón informativo de tu negocio. <br/>Desarrollamos lo que tu negocio necesite; Incluyendo desarrollo de bases de datos, aplicaciones web, aplicaciones mobiles y más.</p>
               </Card.Text>
               <div className="more-btn">
               <Button><span>Conocer más</span> <FontAwesomeIcon icon={faArrowRight}/> </Button>
