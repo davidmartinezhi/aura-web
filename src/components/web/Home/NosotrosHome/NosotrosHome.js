bcdbe47f7839f0042c811c011c5407695348b834
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-06-14 16:05:22 -0500

    first draft nosotros

--- src/components/web/Home/NosotrosHome/NosotrosHome.js
+++ src/components/web/Home/NosotrosHome/NosotrosHome.js
@@ -1,23 +1,37 @@
-import React from 'react'
-import { Container, Row, Col, Button, Card} from "react-bootstrap";
-import testPhoto from "../../../../assets/img/stars.jpg"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faArrowRight} from '@fortawesome/free-solid-svg-icons'
-import './NosotrosHome.scss'
+import React from "react";
+import { Container, Row, Col, Button, Card } from "react-bootstrap";
+import testPhoto from "../../../../assets/img/stars.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import "./NosotrosHome.scss";
 
 export default function NosotrosHome() {
   return (
     <Container fluid className="nosotros-home">
-        <Row className='nosotros-home-titulo pt-5'>
-          <h1>Nosotros</h1>
-        </Row>
-        <Row>
-          <div className="nosotros-home-caption">
-          <p className="m-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-          <Button className=" mt-3 mb-3"><span>Conocer más</span> <FontAwesomeIcon icon={faArrowRight}/> </Button>
-          </div>
-          </Row>
-          {/* <Row className="nosotros-home-stats pt-3">
+      <Row className="nosotros-home-titulo pt-5">
+        <h1>Nosotros</h1>
+      </Row>
+      <Row>
+        <div className="nosotros-home-caption">
+          <p className="m-3">
+            Impulsados por ideas nuevas y atrevidas, con un gran
+            conocimiento tecnológico, y experiencia con clientes de alto perfil.
+            Siempre siguiendo nuestros valores de confianza, respeto, transparencia y
+            coherencia. 
+            Proveemos un conglomerado de servicios redondos que cubren todas las
+            necesidades de nuestros clientes en esta nueva era digital, al
+            brindar soluciones innovadoras y a la medida para desafíos
+            específicos de sus negocios y así colaborar con su óptimo
+            desarrollo.
+
+          </p>
+
+          <Button className=" mt-3 mb-3">
+            <span>Conocer más</span> <FontAwesomeIcon icon={faArrowRight} />{" "}
+          </Button>
+        </div>
+      </Row>
+      {/* <Row className="nosotros-home-stats pt-3">
           <Col  md={3}>
             <Card className="nosotros-card">
               <Card.Body className="nosotros-card-body">
@@ -44,5 +58,5 @@ export default function NosotrosHome() {
           </Col>
         </Row> */}
     </Container>
-  )
+  );
 }
